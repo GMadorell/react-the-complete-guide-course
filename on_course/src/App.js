defe71520ad397eb261c8537b8ed001c593b71dc
GMadorell <gmadorell@gmail.com>
2019-05-01 09:41:44 +0200

    Use radium for modifying hover styling dynamically

--- on_course/src/App.js
+++ on_course/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -57,7 +58,11 @@ class App extends Component {
       font: "inherit",
       border: "2px solid blue",
       padding: "8px",
-      cursor: "pointer"
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
     };
 
     let persons = null;
@@ -79,6 +84,10 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "salmon",
+        color: "black"
+      };
     }
 
     const classes = [];
@@ -102,7 +111,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
 
 /*
  * Version with hooks below
