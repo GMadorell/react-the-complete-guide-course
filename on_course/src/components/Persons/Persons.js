7c552cbb7c9c65ca0e933f138939b9d63519ac2d
GMadorell <gmadorell@gmail.com>
2019-05-05 10:38:07 +0200

    Add a lifecycle hook to persons (shouldComponentUpdate) to optimize cases in which rendering is not needed because state hasn't changed from last render.

--- on_course/src/components/Persons/Persons.js
+++ on_course/src/components/Persons/Persons.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Person from "./Person/Person";
 
-export default props =>
-  props.persons.map((person, index) => {
-    return (
-      <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        changed={event => props.changed(event, person.id)}
-        key={person.id}
-      />
-    );
-  });
+export default class Persons extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.persons !== this.props.persons) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          changed={event => this.props.changed(event, person.id)}
+          key={person.id}
+        />
+      );
+    });
+  }
+}
