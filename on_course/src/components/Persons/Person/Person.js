fb30a7282625973abd279cd3ef6b4936f8689daf
GMadorell <gmadorell@gmail.com>
2019-05-06 08:44:01 +0200

    Use ref in class based component to get the reference to a input element so we can focus on it

--- on_course/src/components/Persons/Person/Person.js
+++ on_course/src/components/Persons/Person/Person.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classes from "./Person.module.css";
 
-const person = props => {
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} and I'm {props.age}
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  inputElementRef = React.createRef();
 
-person.propTypes = {
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
+  render() {
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I'm {this.props.age}
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+          ref={this.inputElementRef}
+        />
+      </div>
+    );
+  }
+}
+
+Person.propTypes = {
   click: PropTypes.func,
   name: PropTypes.string,
   age: PropTypes.number,
   changed: PropTypes.func
 };
 
-export default person;
+export default Person;
