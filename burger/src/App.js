dcb91e1dcdd0c2da0f746dc4f02d392555d0c648
GMadorell <gmadorell@gmail.com>
2019-05-08 09:06:12 +0200

    Add very simply dummy Layout

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import logo from "./logo.svg";
 
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Layout>
+        <p>Lorem ipsum</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
