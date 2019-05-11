7260932ceeaa05630311af4669a2638fe594dc40
GMadorell <gmadorell@gmail.com>
2019-05-11 09:33:46 +0200

    Pass ingredients dynamically to burger as props

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -5,11 +5,22 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from "./Burger.module.css";
 
 const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return (
+          <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        );
+      });
+    }
+  );
+
+  console.log(transformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
