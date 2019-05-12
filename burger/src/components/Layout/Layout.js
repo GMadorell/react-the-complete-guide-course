e9e1df8fe24717cfdb907cd1a5f546204b0ce6d7
GMadorell <gmadorell@gmail.com>
2019-05-12 17:54:51 +0200

    Add side drawer toggler so that we can click on the menu to display the side drawer

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 import classes from "./Layout.module.css";
 
-const layout = props => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>{props.children}</main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  toggleSideDrawerHandler = () => {
+    this.setState(previousState => {
+      showSideDrawer: !previousState.showSideDrawer;
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar drawerTogglerClicked={this.toggleSideDrawerHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Layout;
--- burger/src/components/Navigation/Toolbar/Toolbar.js
+++ burger/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,13 @@ import React from "react";
 
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggler from "../SideDrawer/DrawerToggler/DrawerToggler";
 
 import classes from "./Toolbar.module.css";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggler clicked={props.drawerTogglerClicked} />
     <div className={classes.Logo}>
       <Logo />
     </div>
