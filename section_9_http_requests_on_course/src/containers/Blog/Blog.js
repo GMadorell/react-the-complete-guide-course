1080c7d306c81b01cfbdc66f2f7b425a698cde77
GMadorell <gmadorell@gmail.com>
2019-05-13 09:00:48 +0200

    Transform data when you receive it on a promise when doing http request

--- section_9_http_requests_on_course/src/containers/Blog/Blog.js
+++ section_9_http_requests_on_course/src/containers/Blog/Blog.js
@@ -11,13 +11,17 @@ class Blog extends Component {
 
   componentDidMount() {
     axios.get("http://jsonplaceholder.typicode.com/posts").then(response => {
-      this.setState({ posts: response.data });
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map(post => {
+        return { ...post, author: "Max" };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
 
   render() {
     const posts = this.state.posts.map(post => (
-      <Post key={post.id} title={post.title} />
+      <Post key={post.id} title={post.title} author={post.author} />
     ));
 
     return (
